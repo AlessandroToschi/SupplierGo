739befd441bc52743f3527b576eb4a7b9a01372f
AlessandroToschi <ialessandrotoschi@gmail.com>
2021-01-23 14:50:51 +0100

    SupplierLoader

---
+++ SupplierGoKit/SupplierLoader.swift
@@ -0,0 +1,52 @@
+//
+//  SupplierLoader.swift
+//  SupplierGoKit
+//
+//  Created by Alessandro Toschi on 23/01/2021.
+//
+
+import Foundation
+
+public enum SupplierError: Error{
+    case dataError(DataLoaderError)
+    case noData
+    case invalidData
+}
+
+class SupplierLoader{
+    public let endpoint: URL
+    public private(set) var suppliers: [Supplier]
+    private let jsonDecoder: JSONDecoder
+    
+    public init(endpoint: URL) {
+        self.endpoint = endpoint
+        self.suppliers = []
+        self.jsonDecoder = JSONDecoder()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        
+        self.jsonDecoder.dateDecodingStrategy = .formatted(formatter)
+    }
+    
+    public func load(completionHandler: @escaping (_ error: SupplierError?) -> Void){
+        DataLoader.load(fromURL: self.endpoint) {(data, error) in
+            if let error = error{
+                completionHandler(.dataError(error))
+                return
+            }
+            guard let data = data else{
+                completionHandler(.noData)
+                return
+            }
+            
+            guard let retrievedSupplier = try? self.jsonDecoder.decode([Supplier].self, from: data) else{
+                completionHandler(.invalidData)
+                return
+            }
+            
+            self.suppliers = retrievedSupplier
+            completionHandler(nil)
+        }
+    }
+}
